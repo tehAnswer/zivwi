584e6364fc59ebc4ceb04b73ca34b1c8ebadd2a2
tehAnswer <sergiorodriguezgijon@gmail.com>
2018-11-14 23:29:04 +0100

    Added AuthorizeService (and specs) to issue JWT tokens.

--- authorize_service.go
+++ authorize_service.go
@@ -1,17 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
 // The AuthorizationService is responsible for the user login logic. If a login
 // is successful, the service returns a JWT and by the contrary, an error.
 
 type AuthorizeService interface {
-	login(email string, password string) (string, error)
+	Login(email string, password string) (string, error)
 }
 
 type AuthorizeServiceImpl struct {
-	Users      *UserGateway
+	Users      UserGateway
 	SigningKey []byte
 }
 
 func NewAuthorizeService() AuthorizeService {
-	return nil
+	return &AuthorizeServiceImpl{
+		Users:      NewUserGateway(),
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	}
+}
+
+func (service *AuthorizeServiceImpl) Login(email string, password string) (string, error) {
+	user, loginErr := service.Users.FindBy(email, password)
+
+	if loginErr == nil {
+		return service.buildJwtFor(user)
+	}
+	return "", loginErr
+}
+
+func (service *AuthorizeServiceImpl) buildJwtFor(user *User) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["sub"] = user.Id
+	claims["name"] = fmt.Sprintf("%v %v", user.FirstName, user.LastName)
+	claims["role"] = "user"
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+	// Generate encoded token and send it as response.
+	return token.SignedString(service.SigningKey)
 }
---
+++ authorize_service_test.go
@@ -0,0 +1,61 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/assert"
+	main "github.com/tehAnswer/zivwi"
+)
+
+func TestCorrectLogin(t *testing.T) {
+	service := main.NewAuthorizeService()
+	authorizationService, _ := service.(*main.AuthorizeServiceImpl)
+	user, _ := authorizationService.Users.Create(main.User{
+		FirstName: "Benito",
+		LastName:  "Mussó",
+		Email:     "benito@rome.it",
+		Password:  "cia0p0rc0di0",
+	})
+
+	defer authorizationService.Users.DeleteAll()
+
+	token, loginErr := service.Login("benito@rome.it", "cia0p0rc0di0")
+	if assert.NoError(t, loginErr) {
+		parsedToken, _ := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		assert.True(t, parsedToken.Valid)
+		if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok {
+			assert.Equal(t, user.Id, claims["sub"])
+			assert.Equal(t, "Benito Mussó", claims["name"])
+			assert.Equal(t, "user", claims["role"])
+			assert.NotNil(t, claims["exp"])
+		}
+	}
+
+}
+
+func TestIncorrectLogin(t *testing.T) {
+	service := main.NewAuthorizeService()
+	authorizationService, _ := service.(*main.AuthorizeServiceImpl)
+	authorizationService.Users.Create(main.User{
+		FirstName: "Benito",
+		LastName:  "Mussó",
+		Email:     "benito@rome.it",
+		Password:  "cia0p0rc0di0",
+	})
+
+	defer authorizationService.Users.DeleteAll()
+
+	token, loginErr := service.Login("benito@rome.it", "ciaobella")
+	assert.Empty(t, token)
+	assert.Error(t, loginErr)
+}
